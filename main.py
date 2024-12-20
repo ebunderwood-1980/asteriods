24fd4b4ec14485a14e967eed4ee8a8b7cf23f43d
Cauteryze <e.b.underwood@gmail.com>
2024-12-20 00:38:41 -0800

    Added delta time mechanism

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def main():
     # Initiate pygame
     pygame.init()
     game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,7 @@ def main():
 
         game_screen.fill("BLACK")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
