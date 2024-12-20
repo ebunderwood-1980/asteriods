65a9ed830afdba7c16b353cbebd6c39a9132870e
Cauteryze <e.b.underwood@gmail.com>
2024-12-20 00:17:10 -0800

    Set up virtual environment, constants, gitignore, and main to print a black box

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initiate pygame
+    pygame.init()
+    game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        game_screen.fill("BLACK")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
